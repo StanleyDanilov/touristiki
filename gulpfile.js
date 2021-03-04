939477897eb34872480c3d0cb815fd758d7f7978
StanleyDanilov <dstanleysupreme@gmail.com>
2021-03-04 19:50:05 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
+
+function browsersync() {
+  browserSync.init({
+    server: { baseDir: 'app/' },
+    notify: false,
+  });
+};
+
+function cleanDist() {
+  return del('dist')
+}
+
+function images() {
+  return src('app/images/**/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/images'))
+}
+
+function scripts() {
+  return src([
+    'app/js/main.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
+
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 version'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+};
+
+function buildcopy() {
+  return src([
+    '{app/js,app/css}/*.min.*',
+    'app/fonts/**/*',
+    'app/*.html',
+  ], { base: 'app/' })
+    .pipe(dest('dist'))
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/**/*.min.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
+};
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.buildcopy = series(cleanDist, images, buildcopy);
+exports.default = parallel(styles, scripts, browsersync, watching);

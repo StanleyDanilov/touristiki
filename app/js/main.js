b564e04b89d9f872218b83e2d8603cc52e1777ba
StanleyDanilov <dstanleysupreme@gmail.com>
2021-03-04 23:52:19 +0400

    added new updates

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
-console.log('testing console....');
+let headerProfile = document.querySelector('.header__profile');
+let checkDropMenu = false;
+let dropMenuLinks = document.querySelectorAll('.header__profile-dropmenu__link');
+
+headerProfile.addEventListener('click', toggleDropMenu);
+
+function toggleDropMenu() {
+  let dropMenu = document.querySelector('.header__profile-dropmenu');
+  dropMenu.classList.toggle('header__profile-dropmenu-active');
+  return checkDropMenu = !checkDropMenu
+};
+
+if (checkDropMenu) {
+  window.addEventListener('click', e => {
+    for (let key of dropMenuLinks) {
+      if (!(e.target == key)) {
+        dropMenu.classList.remove('header__profile-dropmenu-active');
+        console.log('123123');
+      }
+    }
+  });
+};
